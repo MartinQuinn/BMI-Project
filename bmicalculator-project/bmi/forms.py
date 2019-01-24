b6a1df1a3f47b351ea998ce4fa7858ae8a23cb2e
martinquinn <martinquinn>
2019-01-24 22:28:45 +0000

    field entries

--- bmicalculator-project/bmi/forms.py
+++ bmicalculator-project/bmi/forms.py
@@ -7,7 +7,7 @@ class BmiForm(forms.Form):
     height is in meters
     weight is in kg
     """
-    name = forms.CharField(required=False)
+    name = forms.CharField(max_length=255, required=False)
     height = forms.FloatField(label="Height in centimeters:", required=True, min_value=0)
     weight = forms.FloatField(label="Weight in kg:", required=True, min_value=0)
 
